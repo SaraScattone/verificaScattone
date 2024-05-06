6522d5884918b900a1f3931cc5e6ccc3c6bbb17c
SaraScattone <sara.scattone@marconirovereto.it>
2024-05-06 08:13:19 +0200

    Primo commit

---
+++ src/main/java/it/marconi/verificascattone/VerificascattoneApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.verificascattone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificascattoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificascattoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/marconi/verificascattone/VerificascattoneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.verificascattone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificascattoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
